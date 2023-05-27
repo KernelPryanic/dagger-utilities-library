2c01f7c5766decd467d1a0368897eb347cd2cfe0
Daniil Trishkin <kernel.pryanic@protonmail.com>
2023-05-27 11:14:27 +0200

    Update filesystem module

--- dul/modules/filesystem.py
+++ dul/modules/filesystem.py
@@ -3,21 +3,20 @@
 import fnmatch
 import os
 
+from typing import Generator
 
-def find_files(path, name: str) -> list[str]:
+
+def find_files(path, name: str) -> Generator[str, None, None]:
     """Find files matching a pattern in a directory tree.
 
     Args:
         path (str): Path to search
         name (str): Filename pattern to match
 
-    Returns:
-        list[str]: List of matching filenames
+    Yields:
+        Generator[str, None, None]: Generator of matching filenames.
     """
 
-    matches: list[str] = []
     for root, _, filenames in os.walk(path):
         for filename in fnmatch.filter(filenames, name):
-            matches.append(os.path.join(root, filename))
-
-    return matches
+            yield os.path.join(root, filename)
